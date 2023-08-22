e58f636ebd03556af395751c94e4feb72110c6b6
Ebenezer Exetie <ebenezeresh525@gmail.com>
2023-08-23 01:18:23 +0300

    delete endpoint api test

--- tests/api/api.spec.js
+++ tests/api/api.spec.js
@@ -88,4 +88,10 @@ test.describe.parallel("API Testing", () => {
         expect(responseBody.job).toBe("new job")
         expect(responseBody.updatedAt).toBeTruthy()
     })
+
+    test("DELETE Request - Delete User", async ({ request }) => {
+        const response = await request.delete(`${baseUrl}/users/2`)
+        
+        expect(response.status()).toBe(204)
+    })
 })
