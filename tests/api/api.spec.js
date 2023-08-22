fc34d50ec58cd9bc33d27de7741553eade8643de
Ebenezer Exetie <ebenezeresh525@gmail.com>
2023-08-23 01:05:36 +0300

    post req test

--- tests/api/api.spec.js
+++ tests/api/api.spec.js
@@ -18,6 +18,30 @@ test.describe.parallel("API Testing", () => {
         expect(response.status()).toBe(200)
 
         const responseBody = JSON.parse(await response.text())
-        console.log(responseBody)
+    })
+
+    test("GET Request - Get User Detail", async ({request}) => {
+        const response = await request.get(`${baseUrl}/users/2`)
+        const responseBody = JSON.parse(await response.text())
+        
+        expect(response.status()).toBe(200)
+        expect(responseBody.data.id).toBe(2)
+        expect(responseBody.data.first_name).toBe("Janet")
+        expect(responseBody.data.last_name).toBe("Weaver")
+        expect(responseBody.data.email).toBeTruthy()
+    })
+
+    test("POST Request - Create New User", async ({ request }) => {
+        const response = await request.post(`${baseUrl}/user`, {
+            data: {
+                id: 1000
+            }
+        })
+
+        const responseBody = JSON.parse(await response.text())
+        
+        expect(response.status()).toBe(201)
+        expect(responseBody.id).toBe(1000)
+        expect(responseBody.createdAt).toBeTruthy()
     })
 })
